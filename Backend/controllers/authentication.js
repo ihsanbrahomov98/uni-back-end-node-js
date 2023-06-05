1e407cc713e769936f8cd2b7269dc881a22dee22
ihsanbrahomov98 <ihsanbrahomov@abv.bg>
2023-06-05 11:54:43 +0300

    fixing register redirect

--- Backend/controllers/authentication.js
+++ Backend/controllers/authentication.js
@@ -45,7 +45,7 @@ router.post("/register", async (req, res) => {
   });
   try {
     const savedUser = await newUser.save();
-    res.status(200).json("user is registered");
+    res.status(200).json(savedUser);
   } catch (error) {
     res.status(500).json("error while registering an user");
   }
